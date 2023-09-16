8c478ce715e5d8e88f7ff5c9060a4ca062ff95cc
seolhi <seolhi1212@gmail.com>
2023-09-16 23:24:01 +0900

    feature: 구독 상품 등록 (S3, 검색, datepicker 등록 모두 완료)

--- src/views/productregister/curation/Curation.style.js
+++ src/views/productregister/curation/Curation.style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { CWidgetStatsF } from "@coreui/react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export const SearchContainer = styled.div`
   display: flex;
@@ -11,3 +13,13 @@ export const SearchImg = styled.img`
   width: 50px;
   height: 50px;
 `;
+
+export const CustomDatePicker = styled(DatePicker)`
+  background-color: #fff;
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+  color: #495057;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+
+`
